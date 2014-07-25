ef656cdb2c9a3eb44a314c0c9522cb538b7cfc75
Rinc <i@rincliu.com>
2014-07-25 13:04:56 +0800

    Pattern matching.

--- 1st.rs
+++ 1st.rs
@@ -5,7 +5,7 @@ fn main() {
 	println!("Hello, {}!", lang);
 	
 	// define mutable variable: 
-	let mut count = 0;
+	let mut count = 0i; // 'count' is an 'int'
 	while count < 10 {
 		println!("count: {}", count);
 		count += 1;// '++' operation is not supported.
@@ -17,11 +17,14 @@ fn main() {
 	// define variable with if-else expression:
 	let x =
 		if is_ten(10) {
-			1
+			1i
 		} else {
-			0
+			0i
 		};
 	println!("x = {}", x);
+
+	let month = 7;
+	println!("The max day of {} is {}", month, max_day(month))
 }
 
 // define function:
@@ -29,4 +32,14 @@ fn is_ten(x: int) -> bool {
 	// No need for a return statement.
 	// The result of the expression is used as the return value. 
 	x == 10
+}
+
+// Pattern matching:
+fn max_day(month: int) -> int {
+	// like 'switch/case' in other languages
+	match month {
+		2 => 28,
+		4 | 6 | 9 | 11 => 30,
+		_ => 31
+	}
 }
