87247aa425d622f7c144a2204f2890bc4ad2de3f
Rinc <i@rincliu.com>
2014-07-29 14:41:38 +0800

    loop, for-in;

--- 1st.rs
+++ 1st.rs
@@ -1,30 +1,22 @@
 fn main() {
 	// define constant variable:
 	let lang = "Rust";
+
 	// print string with variable:
 	println!("Hello, {}!", lang);
-	
-	// define mutable variable: 
-	let mut count = 0i; // 'count' is an 'int'
-	while count < 10 {
-		println!("count: {}", count);
-		count += 1;// '++' operation is not supported.
-	}
 
 	// define type:
 	type MyType = int;
 
-	// define variable with if-else expression:
-	let x =
-		if is_ten(10) {
-			1i
-		} else {
-			0i
-		};
-	println!("x = {}", x);
+	use_if_else();
 
-	let month = 7;
-	println!("The max day of {} is {}", month, max_day(month))
+	use_match();
+
+	use_while();
+
+	use_loop();
+
+	use_for_in();
 }
 
 // define function:
@@ -34,12 +26,57 @@ fn is_ten(x: int) -> bool {
 	x == 10
 }
 
-// Pattern matching:
-fn max_day(month: int) -> int {
-	// like 'switch/case' in other languages
-	match month {
-		2 => 28,
-		4 | 6 | 9 | 11 => 30,
-		_ => 31
+fn use_if_else() {
+	// define variable with if-else expression:
+	let x =
+		if is_ten(10) {
+			1i
+		} else {
+			0i
+		};
+	println!("x = {}", x);
+}
+
+fn use_match() {
+	let month = 7i;
+	// like 'switch/case' in other languages.
+	// like golang, there's no 'falling through'.
+	let max_day =
+		match month {
+			2 => 28i,
+			4 | 6 | 9 | 11 => 30i,
+			_ => 31i
+		};
+	println!("The max day of month({}) is {};", month, max_day);
+}
+
+fn use_while() {
+	// define mutable variable: 
+	let mut count = 0i; // 'count' is an 'int'
+	while count < 10 {
+		println!("count: {}", count);
+		count += 1;// '++' operation is not supported.
+	}
+}
+
+fn use_loop() {
+	let mut x = 100u;
+	//like 'while(true)'
+	loop {
+		x -= 10;
+		println!("x: {}", x);
+		if x <= 0 {
+			break;
+		}
+	}
+}
+
+fn use_for_in() {
+	for y in range(0u, 10) {
+		println!("y: {}", y);
+	}
+	let str = "RincLiu";
+	for chr in str.chars() {
+		println!("{}", chr);
 	}
 }
